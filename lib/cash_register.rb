a9174d9463fbe187d7d88a66491a7753b2833639
Ishmet <ishmetjahan@yahoo.com>
2019-07-01 02:17:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
-
+class CashRegister 
+  
+  attr_accessor :total, :discount
+  
+  def initialize (total)
+    @total= total
+    @discount= discount
+    
+  end
+  
+  def add_item (title, price)
+  
+  end
+  
+  
+  
+  
+end 
